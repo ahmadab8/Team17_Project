31bdfb3f3e4ed0f111812976d647a77f51d2fcbc
baselmahamid <baselme@ac.sce.ac.il>
2021-04-04 13:04:51 +0300

    BSPM2021T17-21

---
+++ lemudim1/classmanager/classmanager/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+from  django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request, 'index.html')
+
