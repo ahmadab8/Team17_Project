6aa07c8e59b435eddd004bf3e064f4f1fcd8b47e
ibrahwem <wattad111@gmail.com>
2021-04-04 16:16:19 +0300

    BSPM2021T17-230 create database for teachers details (connecting with database)

--- lemudim1/classmanager/classroom/models.py
+++ lemudim1/classmanager/classroom/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+
+
 # Create your models here.
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=250)
+    subject = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    username = models.CharField(max_length=250)
+    per_hour = models.CharField(max_length=250)
+    Age = models.CharField(max_length=250)
+    phone = models.CharField(max_length=250)
+
