c925c674a42bf065aea1ae366c845f3e67618d89
baselmahamid <baselme@ac.sce.ac.il>
2021-04-04 17:04:17 +0300

    bug fixing

--- lemudim1/classmanager/classroom/models.py
+++ lemudim1/classmanager/classroom/models.py
@@ -1,17 +1,24 @@
 from django.db import models
-
-
-
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=250)
-    subject = models.CharField(max_length=250)
-    email = models.EmailField(max_length=250)
-    username = models.CharField(max_length=250)
-    per_hour = models.CharField(max_length=250)
-    Age = models.CharField(max_length=250)
-    phone = models.CharField(max_length=250)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=50)#
+    email = models.EmailField(max_length=50)
+    subject = models.CharField(max_length=50)
+    Age = models.CharField(max_length=10)
+    per_hour = models.CharField(max_length=50)
+ #teacher_profile_pic = models.ImageField(upload_to="classroom/teacher_profile_pic", blank=True)
+ #description = models.TextField()
+ #schedule = models.CharField(max_length=250)
+    def __str__(self):
+        return self.name
+
 
