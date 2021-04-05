f59e3a1897af27e9213c941fe1c131a4a7772aa3
ahmadab8 <ahmadab8@ac.sce.ac.il>
2021-04-05 20:06:13 +0300

    TestHomePage 1

--- lemudim1/classmanager/classroom/tests.py
+++ lemudim1/classmanager/classroom/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase,SimpleTestCase
+from django.urls import reverse
+
+
+class HomePageTests(SimpleTestCase):
+    def test_home_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    def test_home_url_name(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertEqual(response.status_code, 200)
+    def test_home_Template_name(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,'index.html')
+    def test_signup_status_code(self):
+        response = self.client.get('/signup/')
+        self.assertEqual(response.status_code, 200)
+    def test_signup_url_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+    def test_signup_Template_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,'signup.html')
+    def test_Studentpage_status_code(self):
+        response = self.client.get('/StudentSignUp/')
+        self.assertEqual(response.status_code, 200)
+    def test_Studentpage_url_name(self):
+        response = self.client.get(reverse('StudentSignUp'))
+        self.assertEqual(response.status_code, 200)
+    def test_Studentpage_Template_name(self):
+        response = self.client.get(reverse('StudentSignUp'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,'StudentSignUp.html')
