36a95f622e2a09c58d1c81652877d19051033a3a
baselmahamid <76214957+baselmahamid@users.noreply.github.com>
2021-04-07 17:03:38 +0300

    updating 1

--- lemudim1/classmanager/classmanager/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-from  django.http import HttpResponse
-from django.shortcuts import render
-
-
-def homepage(request):
-    return render(request, 'index.html')
-
--- lemudim1/classmanager/classroom/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Teacher, Student
-
-admin.site.register(Teacher)
-admin.site.register(Student)
