7e11fb1cf0dc504defa98d571bd206131f503a32
abc <wattad111@gmail.com>
2021-05-23 13:47:30 +0300

    BS_PM2021_TEAM_17BSPM2021T17-207 As an admin i want to see the messages that sent by teachers to students in order to know the difficulties and the relationship between them

--- lemudim1/classmanager/classroom/admin.py
+++ lemudim1/classmanager/classroom/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User,Student,Teacher,message_teach_admin,message_student_admin,MessageToTeacher,StudentsInClass
+from .models import User,Student,Teacher,message_teach_admin,message_student_admin,MessageToTeacher,StudentsInClass,ClassNotice
 # Register your models here.
 
 
 admin.site.register(User, UserAdmin)
+#students list
 admin.site.register(Student)
+#teachers list
 admin.site.register(Teacher)
-
+#messages that sent to admin by teachers
 admin.site.register(message_teach_admin)
+#messages that sent by students to admin
 admin.site.register(message_student_admin)
-
 #msg between boy/girl school and teacher
 admin.site.register(MessageToTeacher)
-
+#msg between teachers and boy/girl school
+admin.site.register(ClassNotice)
 admin.site.register(StudentsInClass)
