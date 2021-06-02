afaae31f38c681eba9ad2e2feaa91cb813b26465
abc <wattad111@gmail.com>
2021-06-02 19:40:40 +0300

    pylint and jenkins

--- lemudim1/classmanager/classroom/models.py
+++ lemudim1/classmanager/classroom/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # pylint: disable=R0903,C0103
 from django.urls import reverse
-import misaka
+#import misaka
 
 # Create your models here.
 
